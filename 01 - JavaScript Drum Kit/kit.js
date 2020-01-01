5daef273944f2ba7d17ff1bdcbe2f8a5106a034c
Bweble <afflexux@gmail.com>
2020-01-01 14:11:06 +0000

    prettier test

--- 01 - JavaScript Drum Kit/kit.js
+++ 01 - JavaScript Drum Kit/kit.js
@@ -12,29 +12,26 @@
 // Better to extracts out above into it's own function as below
 
 function playSound(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); // grabs audio element into audio variable
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); // grabs audio element into audio variable
 
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`); // grabs div element into key variable
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`); // grabs div element into key variable
 
-    if (!audio) return; //Stops function running if no audio key attached
+  if (!audio) return; //Stops function running if no audio key attached
 
-    audio.currentTime = 0; //resets playing time if hit again whilst playing
+  audio.currentTime = 0; //resets playing time if hit again whilst playing
 
-    audio.play(); // plays audio
+  audio.play(); // plays audio
 
-    key.classList.add('playing') // adds class .playing on keydown
+  key.classList.add("playing"); // adds class .playing on keydown
 }
 
-const keys = document.querySelectorAll('.key'); // grabs all .key elements into keys variable
+const keys = document.querySelectorAll(".key"); // grabs all .key elements into keys variable
 
 function removeTransition(e) {
-    if (e.propertyName != 'transform') return;
-    this.classList.remove('playing') // if property name is not transform then remove class .playing
+  if (e.propertyName != "transform") return;
+  this.classList.remove("playing"); // if property name is not transform then remove class .playing
 }
 
-keys.forEach(
-    key => key.addEventListener('transitionend', removeTransition)
-) // listening for transitionend (css event that is fired) then runs the function removeTransition
+keys.forEach(key => key.addEventListener("transitionend", removeTransition)); // listening for transitionend (css event that is fired) then runs the function removeTransition
 
-window.addEventListener('keydown', playSound) // on event run function, much cleaner.
-
+window.addEventListener("keydown", playSound); // on event run function, much cleaner.
